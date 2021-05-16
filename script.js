d2705482933e822227cd959619480ae2cbed23df
Dmitrij Beleckij <dmitrijbeleckij@gmail.com>
2021-05-16 16:08:15 +0200

    paskutinis

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 
@@ -10,8 +11,23 @@ const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
 
+//show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+//hide loading
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+
+}
+
+
 // get quotes from API
 function newQuote() {
+    loading();
     // pisk a reandom quot
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check if author exist
@@ -27,11 +43,13 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
 
     }
-
+    // set wuote, hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -57,5 +75,7 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
+
+
 
 
